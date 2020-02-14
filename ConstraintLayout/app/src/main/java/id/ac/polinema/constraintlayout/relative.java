5cf08908a5ee77c625cbbfee26a9f1a725681fe7
ulvia <ulvia.yulianti@gmail.com>
2020-02-14 08:39:28 +0700

    menambahkan 2 activity baru yaitu relative dan constraint

---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/relative.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class relative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
